87a3e80a9021cd623bc28c466a1c14989d28dfab
SairaMariam <rehmanshafiqur344@gmail.com>
2024-08-22 14:29:15 -0700

    Wrote the program for Seconds_Conversion.py

---
+++ Seconds_Conversion.py
@@ -0,0 +1,4 @@
+total_seconds = int(input("Enter the number of seconds: "))
+minutes = total_seconds // 60
+seconds = total_seconds % 60
+print(f"{total_seconds} seconds is equal to {minutes} minutes and {seconds} seconds.")
