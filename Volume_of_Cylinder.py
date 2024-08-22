c079b2ecc6e5162780052e62c8937683490906b7
SairaMariam <rehmanshafiqur344@gmail.com>
2024-08-22 15:24:24 -0700

    Made the Volume_of_Cylinder.py calculator program

---
+++ Volume_of_Cylinder.py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input("Enter the radius of the cylinder: "))
+height = float(input("Enter the height of the cylinder: "))
+volume = math.pi * (radius ** 2) * height
+print(f"The volume of the cylinder is {volume} cubic units.")
