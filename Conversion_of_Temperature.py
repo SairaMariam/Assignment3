99cfa8f52ca082a7a835338542323bc4a480afb0
SairaMariam <rehmanshafiqur344@gmail.com>
2024-08-22 14:09:50 -0700

    Wrote the program for Coverseion_of_Temperature.py

---
+++ Conversion_of_Temperature.py
@@ -0,0 +1,7 @@
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print(f"{fahrenheit}°F is equal to {celsius}°C.")
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is equal to {fahrenheit}°F.")
